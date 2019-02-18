5a54719c9909bf1e402b4ec8cc550984c7f33c0e
DaMatrix <joey.rabil@gmail.com>
2019-02-18 20:29:52 +0100

    add some dummy files

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#TODO: save version
+echo "Fetching latest NukkitX jar from Jenkins..."
+wget https://ci.nukkitx.com/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+
+echo "Replacing existing jar..."
+rm nukkit.jar
+mv nukkit-1.0-SNAPSHOT.jar nukkit.jar
+echo "Update complete!"
